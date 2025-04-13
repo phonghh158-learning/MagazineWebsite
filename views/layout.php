b473878bc20a45adc787897e10013979eb2bdecf
phonghh.forlearning <phonghh.forlearning@gmail.com>
2025-04-14 03:27:05 +0700

    Add favicon to layout and include favicon.ico file

--- views/layout.php
+++ views/layout.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon/favicon.ico">
     <link rel="stylesheet" href="/assets/css/style.css">
     <?php if (isset($css)) { echo '<link rel="stylesheet" href="'. $css .'">'; } ?>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
